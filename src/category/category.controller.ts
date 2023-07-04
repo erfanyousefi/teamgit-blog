892e5e3ce98c85c2c1099abcbb1cc3cce2296b51
Erfan Yousefi <erfanyousefi.co@gmail.com>
2023-07-05 02:58:35 +0330

    chore: add some field in my model

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -21,7 +21,7 @@ export class CategoryController {
     @Get("/by-name/:name")
     @ApiParam({name: "name", type: "string"})
     async findByName(@Param("name") name: string): Promise<Category> {
-        return this.categoryService.findByName(name);
+        return this.categoryService.findByName(id);
     }
     @Get("/by-id/:id")
     @ApiParam({name: "id", type: "string"})
